e6c0874fe38ab9846e05ef427199e9eca733cbdb
dernestolpz <dernesto.lopez@gmail.com>
2025-01-31 18:25:02 +0000

    ApiKey

--- src/front/js/Firebase/Firebase.js
+++ src/front/js/Firebase/Firebase.js
@@ -6,10 +6,11 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const firebaseApikey = process.env.FIREBASE_API_KEY || "";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCJGwBiJ3nrVyXpSSVgoc3gDlA6w0m9QOs",
+  apiKey: firebaseApikey,
   authDomain: "learnvault-d87c3.firebaseapp.com",
   projectId: "learnvault-d87c3",
   storageBucket: "learnvault-d87c3.firebasestorage.app",
@@ -20,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 const auth = getAuth()
 const db = getFirestore()
