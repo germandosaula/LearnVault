c425aae88b6b70cfaa6e6bcc106f9e71102e98ef
LaHuella <german.dosaula@gmail.com>
2025-02-13 12:32:48 +0000

    no funciona

--- src/api/routes.py
+++ src/api/routes.py
@@ -15,24 +15,24 @@ import bcrypt
 #hola
 load_dotenv()
 
-# private_key = os.getenv("FIREBASE_PRIVATE_KEY")
-# private_key = private_key.replace(r'\n', '\n')
+private_key = os.getenv("FIREBASE_PRIVATE_KEY")
+private_key = private_key.replace(r'\n', '\n')
 
-# cred = credentials.Certificate({
-#     "type": os.getenv("FIREBASE_TYPE"),
-#     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-#     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-#     "private_key": private_key,
-#     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-#     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-#     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-#     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-#     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-#     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
-# })
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": private_key,
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+})
 
-# firebase_admin.initialize_app(cred)
-# print(os.getenv("FIREBASE_PRIVATE_KEY"))
+firebase_admin.initialize_app(cred)
+print(os.getenv("FIREBASE_PRIVATE_KEY"))
 
 api = Blueprint('api', __name__)
 CORS(api, resources={r"/*": {"origins": os.getenv("FRONT_URL"), "allow_headers": ["Authorization", "Content-Type"], "supports_credentials": True}})
